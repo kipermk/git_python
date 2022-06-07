d19173faef7f4a14736f83be80855c5cc933718a
kipermk <kiper3721@gmail.com>
2022-06-07 07:47:09 +0300

    v 2.0

---
+++ ООП/oop.py
@@ -0,0 +1,26 @@
+class dog:
+    legs = 4
+    had = 1
+    voice = 'uff'
+
+    def __init__(self, color, size, name, breed):
+        self.color = color
+        self.size = size
+        self.name = name
+        self.breed = breed
+
+    def events(self):
+        print(self.name, 'Jump')
+        print(self.name, self.voice)
+
+
+bobik = dog('Серый', "Большой", "Бобик", "Хаски-Икея")
+print(bobik.voice)
+bobik.events()
+
+bobik2 = dog('Черный', "Большой", "Миша", "Хаски-Икея")
+bobik2.events()
+
+bobik3 = dog('Черный', "Большой", "Дина", "Хаски-Икея")
+bobik3.voice = 'gaff'
+bobik3.events()
