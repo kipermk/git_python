ddc7bf87ff7c57b26b2a5193eb3188d256b5192e
kipermk <kiper3721@gmail.com>
2022-06-16 09:09:57 +0300

    v 6.1

--- ООП/human.py
+++ ООП/human.py
@@ -1,32 +1,62 @@
-import random
+class Human:
+    default_name = 'Name'
+    default_age = 33
+
+    def __init__(self, name=default_name, age=default_age):
+        self.name = name
+        self.age = age
+        self.__money = 0
+        self.__house = None
+
+    def info(self):
+        print(f"""        name: {self.name} 
+        age: {self.age} 
+        money: {self.__money} 
+        house: {self.__house} """)
+
+    @staticmethod
+    def default_info():
+        print(f'default name: {Human.default_name}')
+        print(f'default age: {Human.default_age}')
+
+    def earn_money(self, how_much):
+        self.__money += how_much
+        print(f'{self.name} has earned {self.__money} $')
+
+    def __make_deal(self, price, house):
+        self.__money -= price
+        self.__house = house
+        print(f'You has bought the {self.__house}, now your money = {self.__money} $')
+
+    def buy_house(self, discount, house):
+        price = house.final_price(discount)
+        if self.__money >= price:
+            self.__make_deal(price, house)
+            print('House is bought')
+        else:
+            print(f'You\'ve got {self.__money} only, house is {price}')
 
 
-class Head:
-    eyes = 2
-    ears = 2
-    mouth = 1
+class House:
+    def __init__(self, area=5, price=500000):
+        self._area = area
+        self._price = price
 
-    def eat(self):
-        taste = 'вкусно' if random.randint(0, 1) else 'не вкусно'
-        print(taste)
-        # return taste
-
-
-class Body:
-    heart = 1
-
-    def sweat(self):
-        sweat_ml = random.randint(10, 1000)
-        # print(f'выделилось {sweat_ml} мл пота')
-        return f'выделилось {sweat_ml} мл пота'
+    def final_price(self, discount):
+        self._price -= discount
+        print(f'Final price with special discount: {self._price}')
+        return self._price
 
 
-class Human(Head, Body):
-    pass
+class SmallHouse(House):
+    def __init__(self, price=100000):
+        House.__init__(self, area=40, price=price)
 
 
-# nick = Human()
-# nick.sweat()
-# nick.eat()
-# print(nick.eyes)
-# print(nick.heart)
+Human.default_info()
+alex = Human(name='alex', age=24)
+alex.info()
+alex.earn_money(100000)
+alex.info()
+sh = SmallHouse()
+alex.buy_house(10, sh)
